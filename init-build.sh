1b3f7adaae4bdadde9836fd3706b51116a4b429b
Patryk Dudziński <dodpat02@gmail.com>
2026-04-15 13:54:17 +0200

    fixed build scripts

--- init-build.sh
+++ init-build.sh
@@ -1,25 +1,12 @@
+#!/bin/bash
+source /opt/ros/jazzy/setup.bash
+
 python3 -m venv .venv && source .venv/bin/activate
-
-# git submodule update --init --recursive
-
-
-
-
-
 python3 -m pip install -r requirements.txt
 pre-commit install
 pre-commit autoupdate
 pre-commit install
 
-
-source /opt/ros/jazzy/setup.bash
-
-colcon build --symlink-install
-source install/setup.bash
-source install/local_setup.bash
-rosdep install --from-paths src --ignore-src -r -y
 rosdep update
-
+rosdep install --from-paths src --ignore-src -r -y
 colcon build --symlink-install
-source install/setup.bash
-source install/local_setup.bash
